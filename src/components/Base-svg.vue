86a790f3090cc63e725fe4657d4a13319f48c9c1
m.fracz <m.fracz@zoovu.com>
2021-03-13 21:26:10 +0100

    create icon components and base icon component

---
+++ src/components/Base-svg.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    :aria-label="name"
+    :class="[`svg-${name}`]"
+    :height="`${height}px`"
+    :viewBox="`0 0 ${width} ${height}`"
+    :width="`${width}px`"
+    role="presentation"
+    :preserveAspectRatio="preserveAspectRatio"
+  >
+    <slot />
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Base-svg",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: Number,
+      required: false
+    },
+    height: {
+      type: Number,
+      required: false
+    },
+    preserveAspectRatio: {
+      type: String || undefined,
+      required: false
+    }
+  }
+});
+</script>
+
+<style scoped></style>
